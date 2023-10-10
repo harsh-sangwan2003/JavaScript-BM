35bcebf643da6b7323b55adede1c6a14afa2381f
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-10-10 16:05:37 +0530

    added

---
+++ 06_Giphy App/scripts/ajax.js
@@ -0,0 +1,18 @@
+function doAjax(name){
+
+    let API = 'r8gNiohp2CBIU3fYs0JjhEKJPf4mNUI3';
+    let baseUrl = `https://api.giphy.com/v1/gifs/search?api_key=${API}&q=${name}&limit=5`;
+
+    let xmlHTTPRequest = new XMLHttpRequest();
+
+    xmlHTTPRequest.onreadystatechange = function(){
+
+        if(this.readyState===4 && this.status===200)
+        console.log(this.responseText);
+
+        console.log(this.readyState);
+    }
+
+    xmlHTTPRequest.open("GET",baseUrl,true);
+    xmlHTTPRequest.send()
+}
